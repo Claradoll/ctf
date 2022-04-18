df37ea5d9002c0c78c3713d4eb4a1d75ea63275f
Claradoll <349465361@qq.com>
2022-04-18 22:02:31 +0800

    add two compose

--- Never_Gonna_Give_You_Up/Never_Gonna_Give_You_Up/src/Never_Gonna_Give_You_Up.php
+++
@@ -1,5 +0,0 @@
-<body background="img/never.jpg" style=" background-repeat:no-repeat ;background-size:100% 100%;
-background-attachment: fixed;">
-<script>alert('你被骗了！');location.href('index.php')</script>
-
-
