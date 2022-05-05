79eb5ad4353f15128334c77c006ec65966dfcbce
Claradoll <349465361@qq.com>
2022-05-05 14:43:46 +0800

    add Reisen

---
+++ Reisen_s_Secret/src/static/Secret.js
@@ -0,0 +1,7 @@
+var flag = '';
+var Encrypted = '';
+
+for (var i = 0 ; i < flag.length ; i++){
+    Encrypted += String.fromCharCode(flag.charCodeAt(i)-10);
+}
+Encrypted = '\\bW]qD&mUkU<\'dZUH[\'i[dUIUi;Yh\'js';
