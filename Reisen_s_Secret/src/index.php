fdc84d18caf0a11d14268893eefc60db37df6fde
Claradoll <349465361@qq.com>
2022-05-05 15:29:53 +0800

    aboilsh F12

--- Reisen_s_Secret/src/index.php
+++ Reisen_s_Secret/src/index.php
@@ -1326,3 +1326,40 @@
 <td bgcolor=888888 colspan=54></td>
 </tr>
 </table></body></html>
+<script>
+
+function nonono(){
+    window.close(); //关闭当前窗口(防抽)
+    window.location="about:blank"; //将当前窗口跳转置空白页
+}
+
+function ck() {
+    console.profile();
+    console.profileEnd();
+    //我们判断一下profiles里面有没有东西，如果有，肯定有人按F12了，没错！！
+    if(console.clear) { console.clear() };
+    if (typeof console.profiles =="object"){
+        return console.profiles.length > 0;
+    }
+}
+
+function hehe(){
+    if( (window.console && (console.firebug || console.table && /firebug/i.test(console.table()) )) || (typeof opera == 'object' && typeof opera.postError == 'function' && console.profile.length > 0)){
+        nonono();
+    }
+    if(typeof console.profiles =="object"&&console.profiles.length > 0){
+        nonono();
+    }
+}
+hehe();
+window.onresize = function(){
+if((window.outerHeight-window.innerHeight)>200)
+//判断当前窗口内页高度和窗口高度，如果差值大于200，那么呵呵
+    nonono();
+}</script>
+<script type="text/javascript">
+function stop() {
+    return false;
+}
+document.oncontextmenu = stop;
+</script>
